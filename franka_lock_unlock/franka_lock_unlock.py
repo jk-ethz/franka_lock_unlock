91d0761dc3fd79e7441f4909051a8c79a6ee2241
Vincenzo Petrone <vipetrone@unisa.it>
2025-05-19 19:37:07 +0200

    [FMT] Apply black formatter to all repo
    - [FMT] Apply other formatters for XML and YAML files
    - [MAK] Add pre-commit hooks configuration

--- franka_lock_unlock/franka_lock_unlock.py
+++ franka_lock_unlock/franka_lock_unlock.py
@@ -20,12 +20,20 @@ from itertools import count
 import atexit
 from threading import Event
 
+
 class FrankaLockUnlock:
-    def __init__(self, hostname: str, username: str, password: str, protocol: str = 'https', relock: bool = False):
+    def __init__(
+        self,
+        hostname: str,
+        username: str,
+        password: str,
+        protocol: str = "https",
+        relock: bool = False,
+    ):
         requests.packages.urllib3.disable_warnings()
         self._session = requests.Session()
         self._session.verify = False
-        self._hostname = f'{protocol}://{hostname}'
+        self._hostname = f"{protocol}://{hostname}"
         self._username = username
         self._password = password
         self._logged_in = False
@@ -46,36 +54,49 @@ class FrankaLockUnlock:
 
     @staticmethod
     def _encode_password(username, password):
-        bs = ','.join([str(b) for b in hashlib.sha256((f'{password}#{username}@franka').encode('utf-8')).digest()])
-        return base64.encodebytes(bs.encode('utf-8')).decode('utf-8')
+        bs = ",".join(
+            [
+                str(b)
+                for b in hashlib.sha256(
+                    (f"{password}#{username}@franka").encode("utf-8")
+                ).digest()
+            ]
+        )
+        return base64.encodebytes(bs.encode("utf-8")).decode("utf-8")
 
     def _login(self):
         print("Logging in...")
         if self._logged_in:
             print("Already logged in.")
             return
-        login = self._session.post(urljoin(self._hostname, '/admin/api/login'), \
-                                           json={'login': self._username, \
-                                                 'password': self._encode_password(self._username, self._password)})
+        login = self._session.post(
+            urljoin(self._hostname, "/admin/api/login"),
+            json={
+                "login": self._username,
+                "password": self._encode_password(self._username, self._password),
+            },
+        )
         assert login.status_code == 200, "Error logging in."
-        self._session.cookies.set('authorization', login.text)
+        self._session.cookies.set("authorization", login.text)
         self._logged_in = True
         print("Successfully logged in.")
 
     def _logout(self):
         print("Logging out...")
         assert self._logged_in
-        logout = self._session.post(urljoin(self._hostname, '/admin/api/logout'))
+        logout = self._session.post(urljoin(self._hostname, "/admin/api/logout"))
         assert logout.status_code == 200, "Error logging out"
         self._session.cookies.clear()
         self._logged_in = False
         print("Successfully logged out.")
 
     def _get_active_token_id(self):
-        token_query = self._session.get(urljoin(self._hostname, '/admin/api/control-token'))
+        token_query = self._session.get(
+            urljoin(self._hostname, "/admin/api/control-token")
+        )
         assert token_query.status_code == 200, "Error getting control token status."
         json = token_query.json()
-        return None if json['activeToken'] is None else json['activeToken']['id']
+        return None if json["activeToken"] is None else json["activeToken"]["id"]
 
     def _is_active_token(self):
         active_token_id = self._get_active_token_id()
@@ -87,18 +108,25 @@ class FrankaLockUnlock:
             assert self._token_id is not None
             print("Already having a control token.")
             return
-        token_request = self._session.post(urljoin(self._hostname, f'/admin/api/control-token/request{"?force" if physically else ""}'), \
-                                           json={'requestedBy': self._username})
+        token_request = self._session.post(
+            urljoin(
+                self._hostname,
+                f'/admin/api/control-token/request{"?force" if physically else ""}',
+            ),
+            json={"requestedBy": self._username},
+        )
         assert token_request.status_code == 200, "Error requesting control token."
         json = token_request.json()
-        self._token = json['token']
-        self._token_id = json['id']
-        print(f'Received control token is {self._token} with id {self._token_id}.')
+        self._token = json["token"]
+        self._token_id = json["id"]
+        print(f"Received control token is {self._token} with id {self._token_id}.")
 
     def _release_token(self):
         print("Releasing control token...")
-        token_delete = self._session.delete(urljoin(self._hostname, '/admin/api/control-token'), \
-                                                    json={'token': self._token})
+        token_delete = self._session.delete(
+            urljoin(self._hostname, "/admin/api/control-token"),
+            json={"token": self._token},
+        )
         assert token_delete.status_code == 200, "Error releasing control token."
         self._token = None
         self._token_id = None
@@ -106,41 +134,67 @@ class FrankaLockUnlock:
 
     def _activate_fci(self):
         print("Activating FCI...")
-        fci_request = self._session.post(urljoin(self._hostname, f'/admin/api/control-token/fci'), \
-                                         json={'token': self._token})
+        fci_request = self._session.post(
+            urljoin(self._hostname, f"/admin/api/control-token/fci"),
+            json={"token": self._token},
+        )
         assert fci_request.status_code == 200, "Error activating FCI."
         print("Successfully activated FCI.")
 
     def _home_gripper(self):
         print("Homing the gripper...")
-        action = self._session.post(urljoin(self._hostname, f'/desk/api/gripper/homing'), \
-                                    headers={'X-Control-Token': self._token})
+        action = self._session.post(
+            urljoin(self._hostname, f"/desk/api/gripper/homing"),
+            headers={"X-Control-Token": self._token},
+        )
         assert action.status_code == 200, "Error homing gripper."
-        print(f'Successfully homed the gripper.')
+        print(f"Successfully homed the gripper.")
 
     def _lock_unlock(self, unlock: bool, force: bool = False):
         print(f'{"Unlocking" if unlock else "Locking"} the robot...')
-        action = self._session.post(urljoin(self._hostname, f'/desk/api/robot/{"open" if unlock else "close"}-brakes'), \
-                                    files={'force': force},
-                                    headers={'X-Control-Token': self._token})
+        action = self._session.post(
+            urljoin(
+                self._hostname,
+                f'/desk/api/robot/{"open" if unlock else "close"}-brakes',
+            ),
+            files={"force": force},
+            headers={"X-Control-Token": self._token},
+        )
         assert action.status_code == 200, "Error requesting brake open/close action."
         print(f'Successfully {"unlocked" if unlock else "locked"} the robot.')
 
-    def run(self, unlock: bool = False, force: bool = False, wait: bool = False, request: bool = False, persistent: bool = False, fci: bool = False, home: bool = False) -> None:
-        assert not request or wait, "Requesting control without waiting for obtaining control is not supported."
+    def run(
+        self,
+        unlock: bool = False,
+        force: bool = False,
+        wait: bool = False,
+        request: bool = False,
+        persistent: bool = False,
+        fci: bool = False,
+        home: bool = False,
+    ) -> None:
+        assert (
+            not request or wait
+        ), "Requesting control without waiting for obtaining control is not supported."
         assert not fci or unlock, "Activating FCI without unlocking is not possible."
-        assert not fci or persistent, "Activating FCI without persistence is not possible."
-        assert not home or unlock, "Homing the gripper without unlocking is not possible."
+        assert (
+            not fci or persistent
+        ), "Activating FCI without persistence is not possible."
+        assert (
+            not home or unlock
+        ), "Homing the gripper without unlocking is not possible."
         self._login()
         try:
-            assert self._token is not None or self._get_active_token_id() is None or wait, "Error requesting control, the robot is currently in use."
+            assert (
+                self._token is not None or self._get_active_token_id() is None or wait
+            ), "Error requesting control, the robot is currently in use."
             while True:
                 self._request_token(physically=request)
                 try:
                     # Consider the timeout of 30 s for requesting physical access to the robot
                     for _ in range(20) if request else count():
                         if (not wait and not request) or self._is_active_token():
-                            print('Successfully acquired control over the robot.')
+                            print("Successfully acquired control over the robot.")
                             self._lock_unlock(unlock=unlock)
                             if home:
                                 self._home_gripper()
@@ -148,9 +202,13 @@ class FrankaLockUnlock:
                                 self._activate_fci()
                             return
                         if request:
-                            print('Please press the button with the (blue) circle on the robot to confirm physical access.')
+                            print(
+                                "Please press the button with the (blue) circle on the robot to confirm physical access."
+                            )
                         elif wait:
-                            print('Please confirm the request message in the web interface on the logged in user.')
+                            print(
+                                "Please confirm the request message in the web interface on the logged in user."
+                            )
                         sleep(1)
                     # In case physical access was not confirmed, try again
                     self._release_token()
@@ -162,28 +220,69 @@ class FrankaLockUnlock:
                 self._logout()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-                                     prog = 'FrankaLockUnlock',
-                                     description = 'Lock or unlock the Franka Emika Panda joint brakes programmatically.',
-                                     epilog = '(c) jk-ethz, https://github.com/jk-ethz'
-                                    )
-    parser.add_argument('hostname', help='The Franka Desk IP address or hostname, for example "1.2.3.4".')
-    parser.add_argument('username', help='The Franka Desk username, usually "admin".')
-    parser.add_argument('password', help='The Franka Desk password.')
-    parser.add_argument('-u', '--unlock', action='store_true', help='Unlock the brakes. Otherwise, lock them.')
-    parser.add_argument('-l', '--relock', action='store_true', help='Relock the brakes on exit.')
-    parser.add_argument('-w', '--wait', action='store_true', help='Wait in case the robot web UI is currently in use.')
-    parser.add_argument('-r', '--request', action='store_true', help='Request control by confirming physical access to the robot in case the robot web UI is currently in use.')
-    parser.add_argument('-p', '--persistent', action='store_true', help='Keep the connection to the robot open persistently.')
-    parser.add_argument('-c', '--fci', action='store_true', help='Activate the FCI.')
-    parser.add_argument('-i', '--home', action='store_true', help='Home the gripper.')
+        prog="FrankaLockUnlock",
+        description="Lock or unlock the Franka Emika Panda joint brakes programmatically.",
+        epilog="(c) jk-ethz, https://github.com/jk-ethz",
+    )
+    parser.add_argument(
+        "hostname",
+        help='The Franka Desk IP address or hostname, for example "1.2.3.4".',
+    )
+    parser.add_argument("username", help='The Franka Desk username, usually "admin".')
+    parser.add_argument("password", help="The Franka Desk password.")
+    parser.add_argument(
+        "-u",
+        "--unlock",
+        action="store_true",
+        help="Unlock the brakes. Otherwise, lock them.",
+    )
+    parser.add_argument(
+        "-l", "--relock", action="store_true", help="Relock the brakes on exit."
+    )
+    parser.add_argument(
+        "-w",
+        "--wait",
+        action="store_true",
+        help="Wait in case the robot web UI is currently in use.",
+    )
+    parser.add_argument(
+        "-r",
+        "--request",
+        action="store_true",
+        help="Request control by confirming physical access to the robot in case the robot web UI is currently in use.",
+    )
+    parser.add_argument(
+        "-p",
+        "--persistent",
+        action="store_true",
+        help="Keep the connection to the robot open persistently.",
+    )
+    parser.add_argument("-c", "--fci", action="store_true", help="Activate the FCI.")
+    parser.add_argument("-i", "--home", action="store_true", help="Home the gripper.")
     args, _ = parser.parse_known_args()
-    assert not args.relock or args.unlock, "Relocking without prior unlocking is not possible."
-    assert not args.relock or args.persistent, "Relocking without persistence would cause an immediate unlock-lock cycle."
+    assert (
+        not args.relock or args.unlock
+    ), "Relocking without prior unlocking is not possible."
+    assert (
+        not args.relock or args.persistent
+    ), "Relocking without persistence would cause an immediate unlock-lock cycle."
 
-    franka_lock_unlock = FrankaLockUnlock(hostname=args.hostname, username=args.username, password=args.password, relock=args.relock)
-    franka_lock_unlock.run(unlock=args.unlock, wait=args.wait, request=args.request, persistent=args.persistent, fci=args.fci, home=args.home)
+    franka_lock_unlock = FrankaLockUnlock(
+        hostname=args.hostname,
+        username=args.username,
+        password=args.password,
+        relock=args.relock,
+    )
+    franka_lock_unlock.run(
+        unlock=args.unlock,
+        wait=args.wait,
+        request=args.request,
+        persistent=args.persistent,
+        fci=args.fci,
+        home=args.home,
+    )
 
     if args.persistent:
         print("Keeping persistent connection...")
--- setup.py
+++ setup.py
@@ -1,26 +1,25 @@
 from setuptools import find_packages, setup
 from glob import glob
 
-package_name = 'franka_lock_unlock'
+package_name = "franka_lock_unlock"
 
 setup(
     name=package_name,
-    version='4.2.1',
-    packages=find_packages(exclude=['test']),
+    version="4.2.1",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', glob('launch/*launch.[pxy][yma]*')),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        ("share/" + package_name + "/launch", glob("launch/*launch.[pxy][yma]*")),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='jk-ethz',
-    maintainer_email='ethz@juliankeller.net',
-    description='Lock or unlock the Franka Emika Panda joint brakes programmatically.',
-    license='AGPLv3',
-    tests_require=['pytest'],
+    maintainer="jk-ethz",
+    maintainer_email="ethz@juliankeller.net",
+    description="Lock or unlock the Franka Emika Panda joint brakes programmatically.",
+    license="AGPLv3",
+    tests_require=["pytest"],
     scripts=[
-        'franka_lock_unlock/franka_lock_unlock.py',
+        "franka_lock_unlock/franka_lock_unlock.py",
     ],
 )
